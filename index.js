ff0f9ad8b0774104347589e24ee605be189ae7a3
pankaj88279 <pankajjawi1993@gmail.com>
2022-08-03 10:37:30 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('hello')
+
+const express=require('express')
+const app=express();
+ 
+const multer=require('multer')
+require('dotenv').config();
+
+app.post('/fileupload',(req,res)=>{
+    console.log(req.body)
+    res.status(201).json({msg:'okk'})
+})
+
+
+
+const port=process.env.port
+app.listen(port,()=>{
+    console.log(`this port on running on port${port}`)
+})
