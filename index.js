fbfccadfdecd8fa1dceca082ee442989df205bbf
pankaj88279 <pankajjawi1993@gmail.com>
2022-08-03 17:19:03 +0530

    done

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ console.log('hello')
 
 const express=require('express')
 const app=express();
- 
+
 const multer=require('multer')
+
 require('dotenv').config();
 
-app.post('/fileupload',(req,res)=>{
-    console.log(req.body)
-    res.status(201).json({msg:'okk'})
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    //console.log(file)
+    let randomNumber = Math.ceil(Math.random()*10000); 
+    let filename = randomNumber+''+file.originalname;
+   
+    cb(null,file.originalname)
+  }
 })
+
+const upload = multer({ storage: storage })
+
+
+app.post('/fileupload', upload.single('myfile'),(req,res)=>{
+  console.log(req.body.name)
+  console.log(req.body.surname)
+  res.status(201).json({msg:'okk'})
+})
+
 
 
 
